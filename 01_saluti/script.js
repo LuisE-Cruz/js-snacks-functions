a7b7334342dbf2d0fdd7a09fbfc61c22cdd0ece6
LuisE Cruz <Luis.cruzlc01@gmail.com>
2025-12-09 16:19:44 +0100

    inizio progetto, prime modifiche al primo esercizio

---
+++ 01_saluti/script.js
@@ -0,0 +1,17 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+let saluto = "Ciao";
+const message = welcome(saluto, userName);
+// Dichiara la funzione qui.
+
+function welcome(saluto, nome){
+    let message = (saluto + "" + userName)
+    return message;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+
+console.log(message);
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
