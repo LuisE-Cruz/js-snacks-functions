27afee193617adea71742252a6ae44450e963ff0
LuisE Cruz <Luis.cruzlc01@gmail.com>
2025-12-09 17:47:59 +0100

    Fine esercizo 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -7,12 +7,20 @@ const userCharacter = 'A'
 // Dichiara la funzione qui.
 
 function oneCharacterArray(array, character){
-     
+
+    const newArray = [];
+    for (let i = 0; i < array.length; i++){
+        if(array[i].charAt(0) === character){
+            newArray.push(array[i])
+        }
+    }
+    
+     return newArray;
 } 
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(oneCharacterArray(names, userCharacter));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
