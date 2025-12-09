18c7c5155aaf1eb036deb5635cb1a2b64b5d4eca
LuisE Cruz <Luis.cruzlc01@gmail.com>
2025-12-09 16:46:01 +0100

    modifiche al primo esercizio, inizio secondo esercizio

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,17 +1,17 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const userName = 'Mario';
-let saluto = "Ciao";
-const message = welcome(saluto, userName);
+
+
 // Dichiara la funzione qui.
 
-function welcome(saluto, nome){
-    let message = (saluto + " " + userName)
+function welcome(name){
+    let message = "Ciao " + name;
     return message;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-console.log(message);
+console.log(welcome(userName));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,20 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+const list2 = ['Paolo']
 
 // Dichiara la funzione qui.
 
+function arrayInitials(array){
+    for(let i = 0; i < array.length; i++){
+
+    }
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
+arrayInitials(names)
+arrayInitials(list2)
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
